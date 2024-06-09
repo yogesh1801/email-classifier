beb200b2d58eee5981de78919a2d6183a564929c
yogesh1801 <114429499+yogesh1801@users.noreply.github.com>
2024-06-09 16:35:13 +0530

    logout fixed

--- src/components/logoutbutton.tsx
+++ src/components/logoutbutton.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "./ui/button";
 
-export default async function LogoutButton () {
+export default function LogoutButton () {
     return <Button variant="outline" className="ml-2" onClick={() =>{
         window.localStorage.removeItem('emails')
         window.location.href = "/api/logout"
